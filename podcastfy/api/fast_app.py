08f3166423821891a9cb9752575f869833d669e1
Bilali ZAKARI <bilal@zakarius.com>
2025-03-16 10:30:39 +0000

    Allow All Origins

--- podcastfy/api/fast_app.py
+++ podcastfy/api/fast_app.py
@@ -43,10 +43,10 @@ def merge_configs(base_config: Dict[Any, Any], user_config: Dict[Any, Any]) -> D
     return merged
 
 app = FastAPI()
-allowed_origins = os.getenv("ALLOWED_ORIGINS", "*").split(",")
+#allowed_origins = os.getenv("ALLOWED_ORIGINS", "*").split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,   
+    allow_origins=["*"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
